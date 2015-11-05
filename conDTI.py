ef7f7b4cb1dc5dc0b13435a34aac5bb4738cfb65
Peter SP Hung <phung17@outlook.com>
2015-11-05 11:11:11 -0500

    Allowed conDTI.py to stand alone by integrating EstimateDTI-masked.sh shell commands into it

--- conDTI.py
+++ conDTI.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os, sys
+import shutil
 from glob import glob
 from optparse import OptionParser
 
@@ -8,15 +9,15 @@ class DWI2DTIMasked:
     def __init__(self, args):
         self.options = options
         self.args = args
+        self.force = False
 
     def startProc(self):
-        print ("\nConverting DWI to DTI with Otsu Masking on: " + args[0])
+        print ("\nConverting DWI to DTI with 0.5 Otsu threshold masking on: " + args[0])
         os.chdir(args[0])
-        self.subjDir = os.getcwd()
-        self.subjName = os.path.basename(os.getcwd())
-        global keep, dire, search
+        global keep, dire, search, force
+        force = self.options.force
         keep = self.options.keep
-        print (options.keep)
+        #print (options.keep)
         dire = str(self.options.dire)
         search = self.options.search
         if not options.dire:
@@ -26,7 +27,7 @@ class DWI2DTIMasked:
 
 
     def searchProc(self):
-        dirs = glob("*"+search+"*")
+        dirs = glob(search)
         if dirs == []:
             print ("\nCannot find any subject folders using *" + str(search) + "* search string. \nPlease try again.\n")
             sys.exit(2)
@@ -34,11 +35,12 @@ class DWI2DTIMasked:
             print ("\nFound: " + str(dirs) + " subject folders")
             for dti in dirs:
                 os.chdir(dti)
-                if os.path.isdir("nifti"):
+                if os.path.isdir("nifti") and not os.path.isfile("doneDTI_Calc.tmp") or os.path.isdir("nifti") and force:
                     os.chdir("nifti")
                     print ("\n-------------------------------------------------------------------------------")
                     print ("\nProcessing: " + os.getcwd())
-                    os.system("EstimateDTI-masked.sh")
+                    #os.system("EstimateDTI-masked.sh")
+                    prog.EstimateDTImasked()
                     if keep == True:
 
                         print ("\nProcessing complete for: " + str(dti))
@@ -54,7 +56,17 @@ class DWI2DTIMasked:
 
 
                     os.chdir("..")
+                    os.system("> doneDTI_Calc.tmp")
+                else:
+                    print ("\n-------------------------------------------------------------------------------")
+                    if os.path.isfile("doneDTI_Calc.tmp"):
+                        print ("\nDWI to DTI conversion is already completed for " + str(dti) + ", moving on")
+                    elif not os.path.isdir("nifti"):
+                        print ("\nnifti folder for " + str(dti) + " does not exist")
+
                 os.chdir("..")
+            print("\n")
+
             os.chdir("..")
 
     def dirProc(self):
@@ -65,11 +77,12 @@ class DWI2DTIMasked:
             sys.exit(2)
         else:
             os.chdir(dire)
-            if os.path.isdir("nifti"):
+            if os.path.isdir("nifti") and not os.path.isfile("doneDTI_Calc.tmp") or os.path.isdir("nifti") and force:
                 os.chdir("nifti")
                 print ("\n-------------------------------------------------------------------------------")
                 print ("\nProcessing: " + os.getcwd())
-                os.system("EstimateDTI-masked.sh")
+                #os.system("EstimateDTI-masked.sh")
+                prog.EstimateDTImasked()
                 if keep == True:
                     print ("\nProcessing complete for: " + str(dire))
                     print ("\n[" + str(os.getcwd()) +"/DTIGen] contains .tmp files.\n")
@@ -83,12 +96,36 @@ class DWI2DTIMasked:
                     print ("\nDTIGen temporary folder removed, processing complete for: " + str(dire) + "\n")
 
                 os.chdir("..")
+                os.system("> doneDTI_Calc.tmp")
+            else:
+                print ("\n-------------------------------------------------------------------------------")
+                if os.path.isfile("doneDTI_Calc.tmp"):
+                    print ("\nDWI to DTI conversion is already completed. Program will now exit.\n")
+                elif not os.path.isdir("nifti"):
+                    print ("\nnifti folder does not exist\n")
+                sys.exit(2)
             os.chdir("..")
 
+    def EstimateDTImasked(self):
+        print ("\nMaking temporary folder for DWI Masking and DTI Estimation [Folder = DTIGen]")
+        if not os.path.isdir("DTIGen"):
+            os.mkdir("DTIGen")
+        os.system("cp DWI_CORRECTED.* DTIGen")
+        os.chdir("DTIGen")
+        print ("\nStart Slicer 4.3.1 DiffusionWeightedVolumeMasking")
+        os.system("Slicer --launch DiffusionWeightedVolumeMasking --echo --removeislands DWI_CORRECTED.nhdr baseline.nrrd mask.nrrd")
+        print ("\nStart Slicer 4.3.1 DWIToDTIEstimation")
+        os.system("Slicer --launch DWIToDTIEstimation --echo --shiftNeg -e LS -m mask.nrrd DWI_CORRECTED.nhdr DTI.nhdr DTI-baseline.nrrd")
+        print ("\nCopying masked and estimated DTI.nhdr")
+        os.system("cp DTI.nhdr ../")
+        os.system("cp DTI.raw.gz ../")
+        os.chdir("..")
+
 if __name__ == '__main__':
-    parser = OptionParser(usage="%prog [options] <subject_dir>")
-    parser.add_option("-s", "--search", dest="search", default='DTI', help="Subdirectory names to search for nifti under. Default: DTI")
-    parser.add_option("-d", "--dti_dir", dest="dire", help="Only process this subdirectory, ignore -s option.")
+    parser = OptionParser(usage="%prog [options] <project directory>")
+    parser.add_option("-s", "--search", dest="search", default='*DTI*', help="Subdirectory names to search for nifti under. Remember to include wildcard characters!\nDefault: *DTI*")
+    parser.add_option("-d", "--dti_dir", dest="dire", help="Only process this subdirectory, ignore -s option")
+    parser.add_option("-f", "--force", action="store_true", dest="force", help="Force run even if already done running on this subject")
     parser.add_option("-k", "--keep", action="store_true", dest="keep", help="Keep .tmp files after conversion. Default: False")
     (options, args) = parser.parse_args()
     if len(args) != 1:
